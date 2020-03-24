4e9eee858beccc2dd94785c594c9eb73d8cbb96b
Noah Kernis <nkernis@gmail.com>
2020-03-24 16:02:07 -0400

    encoder works

--- ButtonMouseControl_pullup/ButtonMouseControl_pullup.ino
+++ ButtonMouseControl_pullup/ButtonMouseControl_pullup.ino
@@ -1,46 +1,55 @@
 
-#include "Mouse.h"
+#include "<Mouse.h>"
+#include <Encoder.h>
 
-// set pin numbers for the five buttons:
+// set pin numbers for the buttons:
 const int upButton = 2;
 const int downButton = 3;
 const int leftButton = 4;
 const int rightButton = 5;
 const int mouseButton = 6;
 
+// set encoder
+Encoder knob(12, 11);
 
-int range = 5;              // output range of X or Y movement; affects movement speed
-int responseDelay = 10;     // response delay of the mouse, in ms
+int lastKnobState = -1;
+int timestamp = 0;
+
+// output range of X or Y movement
+// affects movement speed
+int range = 5;
+int responseDelay = 10;
 
 
 void setup() {
   Serial.begin(9600);
-  
+
   // initialize the buttons' inputs:
   pinMode(upButton, INPUT_PULLUP);
   pinMode(downButton, INPUT_PULLUP);
   pinMode(leftButton, INPUT_PULLUP);
   pinMode(rightButton, INPUT_PULLUP);
   pinMode(mouseButton, INPUT_PULLUP);
-  
+
   // initialize mouse control:
   Mouse.begin();
 }
 
 void loop() {
   // read the buttons:
-  
+
   int upState = digitalRead(upButton);
   int downState = digitalRead(downButton);
   int rightState = digitalRead(rightButton);
   int leftState = digitalRead(leftButton);
   int clickState = digitalRead(mouseButton);
   int rangeState = analogRead(A7);
+  int knobState = knob.read();
 
   //read the value from the pot and map it to range
   range = map(rangeState, 0, 1023, 1, 20);
-  Serial.println(range);
-  
+  // Serial.println(range);
+
 
   // calculate the movement distance based on the button states:
   int  xDistance = (leftState - rightState) * range;
@@ -56,9 +65,25 @@ void loop() {
     // if the mouse is not pressed, press it:
     if (!Mouse.isPressed(MOUSE_LEFT)) {
       Mouse.press(MOUSE_LEFT);
-    } else{
+    } else {
       Mouse.release(MOUSE_LEFT);
     }
+  }
+
+  int knobChange = knobState - lastKnobState;
+  
+  if (abs(knobChange) >= 2) {
+    // get the direction (-1 or 1):
+    int knobDirection = (knobChange / abs(knobChange));
+    
+    int xState = knobDirection * range;
+
+    if (knobDirection != 0) {
+      Mouse.move(xState, yDistance, 0);
+    }
+    
+    // save knobState for next time through loop:
+    lastKnobState = knobState;
   }
 
   // a delay so the mouse doesn't move too fast:
