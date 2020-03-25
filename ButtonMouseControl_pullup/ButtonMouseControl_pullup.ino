1ef164cdf62e8f2e21f045bdb1f310993f380230
Noah Kernis <nkernis@gmail.com>
2020-03-24 20:19:57 -0400

    fix for encoder btn changing x and y direction

--- ButtonMouseControl_pullup/ButtonMouseControl_pullup.ino
+++ ButtonMouseControl_pullup/ButtonMouseControl_pullup.ino
@@ -1,5 +1,20 @@
-
-#include "<Mouse.h>"
+/*
+   Pick And Place HID/USB
+   
+   A HID/USB Device for controlling the mouse. Allows
+   for controlling x and y movement indepeendently, for 
+   accuracy. User can use buttons, rotoraty pads, or a
+   joystick to control each movement. The mouse press
+   "latches" so items can be clicked, moved, then
+   released. The mouse can be set to (x: 0, y: 0) by
+   pressing the "home" button. The number of steps per
+   input can be increased or decreased within the range
+   of 1 to 20 steps.
+   
+   created 02/14/2020
+   by Ben Moll & Noah Kernis
+*/
+#include "Mouse.h"
 #include <Encoder.h>
 
 // set pin numbers for the buttons:
@@ -45,191 +60,191 @@ int knobState;
 
 void setup()
 {
-	Serial.begin(9600);
+  Serial.begin(9600);
 
-	// initialize the button inputs
-	pinMode(upButton, INPUT_PULLUP);
-	pinMode(downButton, INPUT_PULLUP);
-	pinMode(leftButton, INPUT_PULLUP);
-	pinMode(rightButton, INPUT_PULLUP);
-	pinMode(stepUp, INPUT_PULLUP);
-	pinMode(stepDown, INPUT_PULLUP);
-	pinMode(mouseButton, INPUT_PULLUP);
-	pinMode(homeButton, INPUT_PULLUP);
-	pinMode(encoderButton, INPUT_PULLUP);
+  // initialize the button inputs
+  pinMode(upButton, INPUT_PULLUP);
+  pinMode(downButton, INPUT_PULLUP);
+  pinMode(leftButton, INPUT_PULLUP);
+  pinMode(rightButton, INPUT_PULLUP);
+  pinMode(stepUp, INPUT_PULLUP);
+  pinMode(stepDown, INPUT_PULLUP);
+  pinMode(mouseButton, INPUT_PULLUP);
+  pinMode(homeButton, INPUT_PULLUP);
+  pinMode(encoderButton, INPUT_PULLUP);
 
-	// initialize mouse control
-	Mouse.begin();
+  // initialize mouse control
+  Mouse.begin();
 }
 
 void loop()
 {
-	// read buttons
-	upState = digitalRead(upButton);
-	downState = digitalRead(downButton);
-	rightState = digitalRead(rightButton);
-	leftState = digitalRead(leftButton);
-	stepUpState = digitalRead(stepUp);
-	stepDownState = digitalRead(stepDown);
-	clickState = digitalRead(mouseButton);
-	homeState = digitalRead(homeButton);
-	encoderBtnState = digitalRead(encoderButton);
+  // read buttons
+  upState = digitalRead(upButton);
+  downState = digitalRead(downButton);
+  rightState = digitalRead(rightButton);
+  leftState = digitalRead(leftButton);
+  stepUpState = digitalRead(stepUp);
+  stepDownState = digitalRead(stepDown);
+  clickState = digitalRead(mouseButton);
+  homeState = digitalRead(homeButton);
+  encoderBtnState = digitalRead(encoderButton);
 
-	// read encoder
-	knobState = knob.read();
+  // read encoder
+  knobState = knob.read();
 
-	// check if mouse has moved
-	moveMouse();
+  // check if mouse has moved
+  moveMouse();
 
-	// check if mouse has been pressed
-	mousePress();
+  // check if mouse has been pressed
+  mousePress();
 
-	// check if encoder has moved
-	encoderMove();
+  // check if encoder has moved
+  encoderMove();
 
-	// check if homing button has been pressed
-	home();
+  // check if homing button has been pressed
+  home();
 
-	// check if change step up or down has been pressed
-	changeRange();
+  // check if change step up or down has been pressed
+  changeRange();
 
-	// a classic delay
-	delay(responseDelay);
+  // a classic delay
+  delay(responseDelay);
 }
 
 void moveMouse()
 {
-	// calculate the movement distance based on the button states
-	xDistance = (leftState - rightState) * range;
-	yDistance = (upState - downState) * range;
+  // calculate the movement distance based on the button states
+  xDistance = (leftState - rightState) * range;
+  yDistance = (upState - downState) * range;
 
-	// if X or Y is non-zero, move
-	if ((xDistance != 0) || (yDistance != 0))
-	{
-		Mouse.move(xDistance, yDistance, 0);
-	}
+  // if X or Y is non-zero, move
+  if ((xDistance != 0) || (yDistance != 0))
+  {
+    Mouse.move(xDistance, yDistance, 0);
+  }
 }
 
 void mousePress()
 {
-	// if the mouse button is pressed
-	if (clickState == LOW)
-	{
-		// if the mouse is not pressed, press it
-		if (!Mouse.isPressed(MOUSE_LEFT))
-		{
-			Mouse.press(MOUSE_LEFT);
-		}
-		else
-		{
-			Mouse.release(MOUSE_LEFT);
-		}
-	}
+  // if the mouse button is pressed
+  if (clickState == LOW)
+  {
+    // if the mouse is not pressed, press it
+    if (!Mouse.isPressed(MOUSE_LEFT))
+    {
+      Mouse.press(MOUSE_LEFT);
+    }
+    else
+    {
+      Mouse.release(MOUSE_LEFT);
+    }
+  }
 }
 
 void encoderMove()
 {
-	whichDirection();
+  whichDirection();
 
-	if (encoderBtnXYState == 0)
-	{
-		encoderMoveX();
-	}
-	else
-	{
-		encoderMoveY();
-	}
+  if (encoderBtnXYState == 0)
+  {
+    encoderMoveX();
+  }
+  else
+  {
+    encoderMoveY();
+  }
 }
 
 void whichDirection()
 {
-	if (encoderBtnState == LOW)
-	{
-		if (encoderBtnXYState == 0)
-		{
-			encoderBtnXYState == 0;
-		}
-		else
-		{
-			encoderBtnXYState == 1;
-		}
-	}
+  if (encoderBtnState == LOW)
+  {
+    if (encoderBtnXYState == 0)
+    {
+      encoderBtnXYState = 1;
+    }
+    else
+    {
+      encoderBtnXYState = 0;
+    }
+  }
 }
 
 void encoderMoveX()
 {
-	knobChange = knobState - lastKnobStateX;
+  knobChange = knobState - lastKnobStateX;
 
-	if (abs(knobChange) >= 2)
-	{
-		// get the direction (-1 or 1)
-		int knobDirection = (knobChange / abs(knobChange));
+  if (abs(knobChange) >= 2)
+  {
+    // get the direction (-1 or 1)
+    int knobDirection = (knobChange / abs(knobChange));
 
-		int xState = knobDirection * range;
+    int xState = knobDirection * range;
 
-		if (knobDirection != 0)
-		{
-			Mouse.move(xState, yDistance, 0);
-		}
+    if (knobDirection != 0)
+    {
+      Mouse.move(xState, yDistance, 0);
+    }
 
-		// save knobState for next time through loop
-		lastKnobStateX = knobState;
-	}
+    // save knobState for next time through loop
+    lastKnobStateX = knobState;
+  }
 }
 
 void encoderMoveY()
 {
-	knobChange = knobState - lastKnobStateY;
+  knobChange = knobState - lastKnobStateY;
 
-	if (abs(knobChange) >= 2)
-	{
-		// get the direction (-1 or 1)
-		int knobDirection = (knobChange / abs(knobChange));
+  if (abs(knobChange) >= 2)
+  {
+    // get the direction (-1 or 1)
+    int knobDirection = (knobChange / abs(knobChange));
 
-		int yState = knobDirection * range;
+    int yState = knobDirection * range;
 
-		if (knobDirection != 0)
-		{
-			Mouse.move(xDistance, yState, 0);
-		}
+    if (knobDirection != 0)
+    {
+      Mouse.move(xDistance, yState, 0);
+    }
 
-		// save knobState for next time through loop
-		lastKnobStateY = knobState;
-	}
+    // save knobState for next time through loop
+    lastKnobStateY = knobState;
+  }
 }
 
 void home()
 {
-	if (homeState == LOW)
-	{
-		xDistance = 0;
-		yDistance = 0;
+  if (homeState == LOW)
+  {
+    xDistance = 0;
+    yDistance = 0;
 
-		Mouse.move(xDistance, yDistance, 0);
-	}
+    Mouse.move(xDistance, yDistance, 0);
+  }
 }
 
 void changeRange()
 {
-	if (stepUpState == LOW)
-	{
-		if (range > 1)
-		{
-			range = range--;
-		}
-	}
+  if (stepUpState == LOW)
+  {
+    if (range > 1)
+    {
+      range = range--;
+    }
+  }
 
-	if (stepDownState == LOW)
-	{
-		if (range < 20)
-		{
-			range = range++;
-		}
-	}
+  if (stepDownState == LOW)
+  {
+    if (range < 20)
+    {
+      range = range++;
+    }
+  }
 }
 
 void encoderPress()
 {
-	stepUpState = digitalRead(stepUp);
-	stepDownState = digitalRead(stepDown);
+  stepUpState = digitalRead(stepUp);
+  stepDownState = digitalRead(stepDown);
 }
